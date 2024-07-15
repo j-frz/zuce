1f96985f6b931a6f68a5cbca92d3a027bf0ade89
j-frz <j.frz.dev@gmail.com>
2024-07-15 15:59:11 +0100

    test(zuce): establish working functionality

--- __tests__/zuce.test.ts
+++ __tests__/zuce.test.ts
@@ -1,105 +1,124 @@
 import { expect, test } from "@jest/globals";
 
-import { div, start /* $ */ } from "../src";
-// import userEvent from '@testing-library/user-event';
+import { div, start, zuce } from "../src";
+import userEvent from "@testing-library/user-event";
 
-// const user = userEvent.setup();
+const user = userEvent.setup();
 
-// test('div', () => {
-// 	start(() => div().attributes({ id: 'test' }).styles({ background: 'red' }));
+test("styles update", async () => {
+	const getStyleSheet = () => {
+		const styleSheet = window.document.adoptedStyleSheets[0];
+		return Array.from(styleSheet.cssRules).map((cssRule: CSSRule) => {
+			const [selector, rule] = String(cssRule).split(/(?={)/g);
+			return [selector.replace(/[.#]/, ""), rule];
+		});
+	};
 
-// 	const divElement = window.document.getElementById('test');
+	const pink = div().attributes({ id: "pink" }).styles({ color: "pink" });
+	const green = div().attributes({ id: "green" }).styles({ color: "green" });
 
-// 	expect(divElement).toBeDefined();
+	const isBlue = zuce(false);
 
-// 	const [styleSheet] = getStyleSheet();
-// 	console.log('style 1 =>', styleSheet);
-// });
-// test('div', () => {
-// 	start(() => div().attributes({ id: 'test' }).styles({ background: 'green' }));
+	const redOrBlue = isBlue
+		.div()
+		.attributes({ id: "red-or-blue" })
+		.events({
+			click: () => {
+				isBlue.value = true;
+			},
+		})
+		.styles(({ s }) => {
+			return { color: s ? "blue" : "red" };
+		});
 
-// 	const divElement = window.document.getElementById('test');
+	start(() => div(pink, green, redOrBlue));
 
-// 	expect(divElement).toBeDefined();
+	const pinkDiv = window.document.getElementById("pink");
+	const greenDiv = window.document.getElementById("green");
+	const redOrBlueDiv = window.document.getElementById("red-or-blue");
 
-// 	const [styleSheet] = getStyleSheet();
-// 	console.log('style 2 =>', styleSheet);
-// });
-// test('div', () => {
-// 	start(() => div().attributes({ id: 'test' }).styles({ background: 'blue' }));
+	const [[pinkSelector, pinkRule], [greenSelector, greenRule], [redSelector, redRule]] =
+		getStyleSheet();
 
-// 	const divElement = window.document.getElementById('test');
+	expect(pinkDiv).toBeDefined();
+	expect(pinkDiv?.classList.toString()).toMatch(pinkSelector);
+	expect(pinkRule).toMatch("{color:pink;}");
 
-// 	expect(divElement).toBeDefined();
+	expect(greenDiv).toBeDefined();
+	expect(greenDiv?.classList.toString()).toMatch(greenSelector);
+	expect(greenRule).toMatch("{color:green;}");
 
-// 	const [styleSheet] = getStyleSheet();
-// 	console.log('style 3 =>', styleSheet);
-// });
+	expect(redOrBlueDiv).toBeDefined();
+	expect(redOrBlueDiv?.classList.toString()).toMatch(redSelector);
+	expect(redRule).toMatch("{color:red;}");
 
-// test('styling update on state update', async () => {
-// 	const isBlue = $(false);
+	user.click(redOrBlueDiv as Element);
 
-// 	const red = isBlue
-// 		.div()
-// 		.attributes({ id: 'red' })
-// 		.events({
-// 			click: () => {
-// 				isBlue.value = true;
-// 			},
-// 		})
-// 		.styles(({ s }) => {
-// 			return { color: s ? 'blue' : 'red' };
-// 		});
+	await new Promise((r) => setTimeout(r, 100));
 
-// 	start(() => div(red) as any);
+	const [, , , [blueSelector, blueRule]] = getStyleSheet();
 
-// 	const [firstRule_test_1, secondRule_test_1] = getStyleSheet();
-
-// 	const redOrBlueDiv = window.document.getElementById('red');
-
-// 	expect(redOrBlueDiv).toBeDefined();
-// 	expect(firstRule_test_1).toMatch('._001{color:red;}');
-// 	expect(secondRule_test_1).toBe(undefined);
-
-// 	user.click(redOrBlueDiv);
-
-// 	await new Promise((r) => setTimeout(r, 2000));
-
-// 	const [styleSheet] = window.document.adoptedStyleSheets;
-// 	styleSheet.cssRules;
-
-// 	const [firstRule_test_2, secondRule_test_2] = getStyleSheet();
-// 	expect(firstRule_test_2).toMatch('._001{color:red;}');
-// 	expect(secondRule_test_2).toMatch('._002{color:blue;}');
-// });
-
-test("basic styling", () => {
-	const red = div().attributes({ id: "red" }).styles({ color: "pink" });
-	const blue = div().attributes({ id: "blue" }).styles({ color: "green" });
-
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	start(() => div(red, blue) as any);
-
-	const redDiv = window.document.getElementById("red");
-	const blueDiv = window.document.getElementById("blue");
-
-	const [[firstSelector, firstRule], [secondSelector, secondRule]] = getStyleSheet();
-
-	expect(redDiv).toBeDefined();
-	expect(blueDiv).toBeDefined();
-	expect(redDiv.classList.toString()).toMatch(firstSelector);
-	expect(blueDiv.classList.toString()).toMatch(secondSelector);
-
-	expect(firstRule).toMatch("{color:pink;}");
-
-	expect(secondRule).toMatch("{color:green;}");
+	expect(redOrBlueDiv).toBeDefined();
+	expect(redOrBlueDiv?.classList.toString()).toMatch(blueSelector);
+	expect(blueRule).toMatch("{color:blue;}");
 });
 
-const getStyleSheet = () => {
-	const styleSheet = window.document.adoptedStyleSheets[0];
-	console.log("asd", styleSheet.cssRules);
-	return Array.from(styleSheet.cssRules).map((cssRule: CSSRule) => {
-		const [selector, rule] = String(cssRule).split(/(?={)/g);
-		return [selector.replace(/[.#]/, ""), rule];
+test("attributes update", async () => {
+	const isBlue = zuce(false);
+
+	const redOrBlue = isBlue
+		.div()
+		.attributes(({ s }) => ({ id: s ? "blue" : "red" }))
+		.events({
+			click: () => {
+				isBlue.value = true;
+			},
+		});
+
+	start(() => div(redOrBlue));
+
+	const redDiv = window.document.getElementById("red");
+	expect(redDiv).toBeDefined();
+	expect(redDiv?.attributes.getNamedItem("id")?.value).toMatch("red");
+
+	user.click(redDiv as Element);
+
+	await new Promise((r) => setTimeout(r, 100));
+
+	const blueDiv = window.document.getElementById("blue");
+	expect(blueDiv).toBeDefined();
+	expect(blueDiv).toEqual(redDiv);
+});
+
+test("events update", async () => {
+	const clickAssertFalse = jest.fn(() => {
+		isBlue.value = true;
 	});
-};
+	const clickAssertTrue = jest.fn(() => {
+		isBlue.value = false;
+	});
+
+	const isBlue = zuce(false);
+
+	const redOrBlue = isBlue
+		.div()
+		.attributes({ id: "ZUCE" })
+		.events(({ s }) => ({ click: s ? clickAssertTrue : clickAssertFalse }));
+
+	start(() => div(redOrBlue));
+
+	const redDiv = window.document.getElementById("ZUCE");
+	expect(redDiv).toBeDefined();
+
+	user.click(redDiv as Element);
+
+	await new Promise((r) => setTimeout(r, 100));
+
+	expect(clickAssertFalse).toBeCalled();
+
+	user.click(redDiv as Element);
+
+	await new Promise((r) => setTimeout(r, 100));
+
+	expect(clickAssertTrue).toBeCalled();
+});
